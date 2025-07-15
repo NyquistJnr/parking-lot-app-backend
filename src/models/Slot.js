c8ac1c1ec37f669df65f8bf8bb56bc2b0f45c012
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2025-07-15 17:21:49 +0100

    Cleared the project.

--- src/models/Slot.js
+++
@@ -1,9 +0,0 @@
-// src/models/Slot.js
-const mongoose = require("mongoose");
-
-const slotSchema = new mongoose.Schema({
-  slotNumber: { type: String, required: true, unique: true },
-  isAvailable: { type: Boolean, default: true },
-});
-
-module.exports = mongoose.model("Slot", slotSchema);
