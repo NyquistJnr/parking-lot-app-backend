c0ca164cf74fd231c46d3a5e802e558cc57aca99
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2025-07-15 17:42:47 +0100

    Exported the App.

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 module.exports = app;
